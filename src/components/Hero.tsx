1f5e9a205ad583ad0819026e6c4c29b67924bd31
nadeemjn <partonline34@gmail.com>
2024-11-09 12:58:32 +0500

    Fix unescaped single quote in Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,27 +2,25 @@ import React from "react";
 import { Navbar } from "./Navbar";
 
 export const Hero = () => {
-  return (
-    <div
-      id="hero"
-      className="min-h-screen bg-no-repeat bg-[url(/nadeem.jpg)] bg-left lg:bg-[15%] bg-cover"
-      style={{ backgroundSize: "35%" }}
-    >
-      <div>
-        <Navbar />
-        <div>
-          <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
-            <div className="hidden lg:block"></div>
-            <div className="text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center">
-              <div>
-                <p data-aos="fade-down-left">I'm</p>
-                <p data-aos="fade-down-left">Nadeem</p>
-                <p data-aos="fade-down-left">Ali</p>
-              </div>
+    return (
+        <div id="hero" className="min-h-screen bg-no-repeat bg-[url(/nadeem.jpg)] bg-left lg:bg-[15%] bg-cover"
+            style={{ backgroundSize: "35%" }}
+        >
+            <div>
+                <Navbar />
+                <div>
+                    <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
+                        <div className="hidden lg:block"></div>
+                        <div className="text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center">
+                            <div>
+                                <p data-aos="fade-down-left">I&apos;m</p>
+                                <p data-aos="fade-down-left">Nadeem</p>
+                                <p data-aos="fade-down-left">Ali</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
